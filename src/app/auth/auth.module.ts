33d072fdee12c0652c5de882eadf9019273fb04a
Jovana Mitrovic <49660657+mitrovicjovana@users.noreply.github.com>
2022-03-11 07:33:26 +0100

    Add sign up page

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
 import { AuthComponent } from './pages/auth/auth.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [AuthComponent, LoginComponent, SignUpComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, SharedModule],
 })
 export class AuthModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, FlexLayoutModule, MatButtonModule],
-  exports: [CommonModule, FlexLayoutModule, MatButtonModule],
+  imports: [CommonModule, FlexLayoutModule, MatButtonModule, MatInputModule],
+  exports: [CommonModule, FlexLayoutModule, MatButtonModule, MatInputModule],
 })
 export class SharedModule {}
